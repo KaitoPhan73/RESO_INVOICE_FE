dcda415f1eff330a91178b518fd8368fb73eebc1
CrisBryann <nguyenphuocthienan2018cm@gmail.com>
2024-05-29 02:00:49 +0700

    fetch organization api

---
+++ src/types/Partner.ts
@@ -0,0 +1,10 @@
+export type Partner = {
+  id: string;
+  name: string;
+  description: string;
+  apiUrl: string;
+  type: number;
+  environment: number;
+  status: number;
+  schemaConfig: string;
+};
